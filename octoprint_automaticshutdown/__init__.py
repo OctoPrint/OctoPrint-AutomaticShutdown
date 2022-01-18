bb1fe79b340b2f4d22c02098b5c026dde0b1f712
jneilliii <jneilliii+github@gmail.com>
2022-01-18 12:37:09 -0500

    Update __init__.py

--- octoprint_automaticshutdown/__init__.py
+++ octoprint_automaticshutdown/__init__.py
@@ -166,7 +166,7 @@ class AutomaticshutdownPlugin(octoprint.plugin.TemplatePlugin,
 		self._logger.info("Shutting down system with command: {command}".format(command=shutdown_command))
 		try:
 			import sarge
-			p = sarge.run(shutdown_command, async=True)
+			p = sarge.run(shutdown_command, async_=True)
 		except Exception as e:
 			self._logger.exception("Error when shutting down: {error}".format(error=e))
 			return
